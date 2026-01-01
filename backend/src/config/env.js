83064001d7543f89fe1741e81804b6b48c00b46b
Shekeeb <shekeebmohammed10@gmail.com>
2026-01-01 11:19:53 +0530

    db connection & tools setup

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,8 +2,14 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const ENV={
-    NODE_ENV:process.env.NODE_ENV,
-    PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL
+export const ENV = {
+    NODE_ENV: process.env.NODE_ENV || development,
+    PORT: process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL,
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
 }
